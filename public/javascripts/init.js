772419ae4a326a0d8d21e53edad75ad213bc56a9
Emil <s134873@student.dtu.dk>
2017-01-25 21:27:58 +0100

    trawdawf

---
+++ public/javascripts/init.js
@@ -0,0 +1,12 @@
+﻿var hello = 'Would you like to know a secret?';
+var yes = 'Okay then. Meet me in noerdenirvana.dk/secret';
+var no = '';
+
+$(document).ready(function () {
+    console.log('Hello, Friend...');
+    $('#info').show('medium');
+});
+
+window.onbeforeunload = function () {   
+    $('#info').hide('medium')
+}
