05b98588a44ebebea660e2f93c64529193961b03
Emil <s134873@student.dtu.dk>
2017-01-25 21:40:26 +0100

    new port

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP, || "127.0.0.1")
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP, || "127.0.0.1");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
