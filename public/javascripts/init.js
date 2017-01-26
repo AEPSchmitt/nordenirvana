8a17e2332404aa25072877738d235826b9cc4cb7
Adam <s134873@studen.dtu.dk>
2017-01-26 12:06:45 +0100

    Design shit

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,12 +1,23 @@
 ﻿var hello = 'Would you like to know a secret?';
 var yes = 'Okay then. Meet me in noerdenirvana.dk/secret';
-var no = '';
+var no = 'Are you fucking kidding me?';
 
 $(document).ready(function () {
     console.log('Hello, Friend...');
-    $('#info').show('medium');
+    $('.left').fadeIn('medium');
 });
 
 window.onbeforeunload = function () {   
-    $('#info').hide('medium')
+    $('.info').hide('medium')
+}
+
+var open = 'none';
+function openMenu(menu_name){
+	console.log($(menu_name));
+	if (open != 'none'){
+		$(open).hide('medium');
+	}
+	$(menu_name).show('medium');
+	open = menu_name;
+	console.log(open);
 }
